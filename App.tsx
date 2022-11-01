8a90b5eb3dd6e1a639a41cfa1a073a697bf1f4a5
evelynhelena <evelynhelena781@gmail.com>
2022-11-01 07:36:20 -0300

    WIP: configurando layout da aplicação

--- App.tsx
+++ App.tsx
@@ -1,28 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { NativeBaseProvider } from "native-base";
-
+import { NativeBaseProvider , StatusBar} from "native-base";
+import {THEME} from './src/styles/theme';
+import {useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold} from "@expo-google-fonts/roboto";
+import { Loading } from './src/components/Loading';
+import { SignIn } from './src/screens/SignIn';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_500Medium, Roboto_700Bold});
   return (
-    <NativeBaseProvider>
-      <View style={styles.container}>
-        <Text style={styles.title}>Ola mundo</Text>
-        <StatusBar style="auto" />
-      </View>
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
+        {fontsLoaded ? <SignIn/> : <Loading/>}
     </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    color: '#fff',
-    fontSize: 20,
-  }
-});
